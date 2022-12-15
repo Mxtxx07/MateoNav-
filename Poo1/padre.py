31eb9d0c7557dc60be0c8cbb699a5c268102a20e
SIGA-SA <zagor_kesi@hotmail.com>
2022-12-15 07:57:06 -0500

    cambios

--- Poo1/padre.py
+++ Poo1/padre.py
@@ -1,46 +1,73 @@
+#Herencia de Python
+
 class Persona:
     nombre   = str
     apellido = str
+    edad     = int
+    
+    def __init__(self,nombre,apellido,edad):
+        self.nombre   = nombre
+        self.apellido = apellido
+        self.edad     = edad
+        
+    def conversar(self,otra_persona):
+        return f"Hola{otra_persona.nombre}, me llamo {self.nombre}, tengo {self.edad} años"
+    
+    
+class Hijo (Persona):
+    ciudad = str
+    
+    def __init__(self, nombre, apellido, edad, ciudad):
+       Persona.__init__(self, nombre, apellido, edad)
+       self.ciudad = ciudad 
+       
+padre = Persona (" Victor", "Grados", 50)
+print(vars(padre))
 
-    def __init__(self, nombre, apellido):
+hijo = Hijo(" Elena", "Grados", 20, "Quito")
+print(vars(hijo))
+
+
+print(padre.conversar(hijo))
+print(hijo.conversar(padre))
+
+class Persona2:
+    nombre   = str
+    apellido = str
+    edad     = int
+    
+    def __init__(self,nombre,apellido):
         self.nombre   = nombre
         self.apellido = apellido
         
-    def imprimir(self):
-        print(self.nombre, self.apellido)
-
-x = Persona("Alexander", "Flores")
-x.imprimir()
-
-class Studiante(Persona):
-    pass
-
-y = Studiante("Jerremi", "Cañizares")
-y.imprimir()
-
-
-class Student(Persona):
-    edad =  int
+    def conversar(self,otra_persona):
+        return f"Hola{otra_persona.nombre}, me llamo {self.nombre}, tengo {self.edad} años"
     
-    def __init__(self, nombre, apellido, edad):
-        Persona.__init__(self,nombre, apellido)
-        self.edad = edad
-
-estudiante1 = Student("Carlos", "Dell", 30)
-estudiante1.imprimir()
-
-
-class Student1(Persona):
-    edad     = int
-    semestre = str
+class Hijo2(Persona2):
+    estudios      = str
+    lugarEstudios = str
     
-    def __init__(self, nombre, apellido, edad, semestre):
+    def __init__(self, nombre, apellido, materia, lugarEstudios):
         super().__init__(nombre, apellido)
-        self.edad     = edad
-        self.semestre = semestre
+        self.materia       = materia
+        self.lugarEstudios = lugarEstudios
+        
+    def informar(self, otra_persona):
+        return f"Buenas tardes,{otra_persona.nombre} acabo de estudiar {self.materia} llegue hace unos 20 minutos de {self.lugarEstudios} "
     
-    def bienvenido(self):
-        print("Bienvenido " + self.apellido + " al " + self.semestre + " ingresas a los " + str(self.edad) + " años")
+padre2 = Persona2("Juan", "Flores")
+hijo2  = Hijo2("Jose", "Perez", "Programacion OO", "ITS Yavirac")
 
-p5 = Student1("Diego", "Yanez", 29, "Segundo")
-p5.bienvenido()
+print(hijo2.informar(padre2))
+
+class Padre:
+     nombre   = str
+     apellido = str
+     edad     = int
+     ciudad   = str
+    
+     def __init__(self, nombre, apellido, edad, ciudad):
+        self.nombre   = nombre
+        self.apellido = apellido
+        self.edad     = edad
+        self.ciudad   = ciudad
