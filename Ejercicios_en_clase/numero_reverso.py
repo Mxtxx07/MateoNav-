28cf8f01cd96dea268dfba63fd73fdeb37d25513
SIGA-SA <zagor_kesi@hotmail.com>
2022-12-18 18:25:04 -0500

    Uber

--- Ejercicios_en_clase/numero_reverso.py
+++
@@ -1,9 +0,0 @@
-x = 10
-while True:
-    x -= 1
-    print(x) 
-    if x == 0:
-        break
-else:
-    
-    print("Final")
--- uber/paymentCash.py
+++
@@ -1,5 +0,0 @@
-from payment import Payment
-
-class PaymentCash(Payment):
-    def __init__(self, id, valor, fecha):
-        super().__init__(id, valor, fecha)
